c49109bd36cc904ed3bbc6599b8d70fc56057cb0
mehulkapadia <mehulkapadia@c02g45g9md6r>
2023-04-22 15:56:25 -0400

    Github fullstack pipeline initial code.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# fullstack-pipeline
-Repository for GitHub Actions Demo Project
+# How to test Spring Boot Application, Controllers and Services
+
+1. [Testing with Spring Boot 2.4 and JUnit 5] demonstrating GitHub Actions Workflow.
+
+ 
---
+++ src/main/java/com/kaps/employees/dao/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.kaps.employees.dao;
+
+import com.kaps.employees.model.Employee;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
+
+}
