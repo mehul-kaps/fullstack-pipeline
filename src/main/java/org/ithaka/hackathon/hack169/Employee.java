4aa5e62ffade0bff6264eb3399aeb59f0b5108df
mehulkapadia <mehulkapadia@mehuls-mbp-2>
2023-09-27 19:03:40 -0400

    BRANCH_COVERAGE test

---
+++ src/main/java/org/ithaka/hackathon/hack169/Employee.java
@@ -0,0 +1,45 @@
+package org.ithaka.hackathon.hack169;
+
+import java.util.Objects;
+
+public class Employee {
+
+	String name;
+	String address;
+	Integer age;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(address, age, name);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		return Objects.equals(address, other.address) && Objects.equals(age, other.age)
+				&& Objects.equals(name, other.name);
+	}
+}
--- src/main/java/org/ithaka/hackathon/hack169/StringUtils.java
+++ src/main/java/org/ithaka/hackathon/hack169/StringUtils.java
@@ -5,4 +5,8 @@ public class StringUtils {
 	public static Integer len(String in) {
 		return in.length();
 	}
+	
+	public static String merge(String str1, String str2) {
+		return str1 + str2;
+	}
 }
