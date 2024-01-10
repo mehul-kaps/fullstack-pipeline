db3a6573ef2f70c67a73059f2cc14a04bcb67bc8
mehulkapadia <mehulkapadia@rocinante>
2024-01-10 18:52:25 -0500

    New JS script changes for jacoco report.

--- .github/scripts/comment-jacoco-junit.js
+++ .github/scripts/comment-jacoco-junit.js
@@ -1,18 +1,18 @@
-const fs = require('fs');
+import { readFileSync } from 'fs';
 
 const artifactPath = 'target/site/jacoco';
 const coverageReportPath = `${artifactPath}/index.html`;
 
 try {
-  const coverageReport = fs.readFileSync(coverageReportPath, 'utf-8');
-  const commentBody = `## Jacoco JUnit Coverage Report\n\n${coverageReport}`;
+	const coverageReport = readFileSync(coverageReportPath, 'utf-8');
+	const commentBody = `## Jacoco JUnit Coverage Report\n\n${coverageReport}`;
 
-  await github.rest.issues.createComment({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    issue_number: context.issue.number,
-    body: commentBody,
-  });
+	await github.rest.issues.createComment({
+		owner: context.repo.owner,
+		repo: context.repo.repo,
+		issue_number: context.issue.number,
+		body: commentBody,
+	});
 } catch (error) {
-  console.error(`Error reading Jacoco report: ${error.message}`);
+	console.error(`Error reading Jacoco report: ${error.message}`);
 }
