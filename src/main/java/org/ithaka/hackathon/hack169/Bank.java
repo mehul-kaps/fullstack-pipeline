5a6713b554f7402f4ff4fc2eb3c2818feaef42e1
mehulkapadia <mehulkapadia@rocinante>
2024-01-29 09:23:25 -0500

    test

--- src/main/java/org/ithaka/hackathon/hack169/Bank.java
+++ src/main/java/org/ithaka/hackathon/hack169/Bank.java
@@ -6,10 +6,10 @@ public class Bank {
 		System.out.println("Deposited [" + amount + "] to account [" + ac + "]");
 	}
 	
-	public void withdraw(Integer ac, Double amount) {
-		if(amount < 50)
-			System.out.println("Cannot without less than $50");
-		else
-			System.out.println("Withdrawn [" + amount + "] from account [" + ac + "]");
-	}
+//	public void withdraw(Integer ac, Double amount) {
+//		if(amount < 50)
+//			System.out.println("Cannot without less than $50");
+//		else
+//			System.out.println("Withdrawn [" + amount + "] from account [" + ac + "]");
+//	}
 }
--- src/main/java/org/ithaka/hackathon/hack169/StringUtils.java
+++ src/main/java/org/ithaka/hackathon/hack169/StringUtils.java
@@ -11,15 +11,15 @@ public class StringUtils {
 		return in.length();
 	}
 	
-	public static String merge(String str1, String str2) {
-		return str1 + str2;
-	}
-	
-	public static Boolean hasText(String str) {
-		return org.springframework.util.StringUtils.hasText(str);
-	}
-	
-	public static Boolean hasText_new(String str) {
-		return org.springframework.util.StringUtils.hasText(str);
-	}
+//	public static String merge(String str1, String str2) {
+//		return str1 + str2;
+//	}
+//	
+//	public static Boolean hasText(String str) {
+//		return org.springframework.util.StringUtils.hasText(str);
+//	}
+//	
+//	public static Boolean hasText_new(String str) {
+//		return org.springframework.util.StringUtils.hasText(str);
+//	}
 }
